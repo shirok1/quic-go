218a69efe06c2833ae79b8f71eae0c6f5147c17a
Shiroki Satsuki <me@shirok1.dev>
2025-04-17 22:28:49 +0800

    ioutil deprecated

--- example/main.go
+++ example/main.go
@@ -6,7 +6,6 @@ import (
 	"flag"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
 	"mime/multipart"
 	"net/http"
@@ -60,7 +59,7 @@ func init() {
 	})
 
 	http.HandleFunc("/demo/echo", func(w http.ResponseWriter, r *http.Request) {
-		body, err := ioutil.ReadAll(r.Body)
+		body, err := io.ReadAll(r.Body)
 		if err != nil {
 			fmt.Printf("error reading body while handling /echo: %s\n", err.Error())
 		}
